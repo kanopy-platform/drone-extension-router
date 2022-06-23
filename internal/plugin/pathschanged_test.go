025e1ec3a0fdb32da8bb820db5abdfd07f2e17e3
Colin Hoglund <colinhoglund@users.noreply.github.com>
2022-06-23 13:05:42 -0700

    Update internal/plugin/pathschanged_test.go
    Co-authored-by: David Katz <david.si.katz@gmail.com>

--- internal/plugin/pathschanged_test.go
+++ internal/plugin/pathschanged_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 )
 
-func TestPathsChanged(t *testing.T) {
-	// assert that PathsChanged fulfills the converter.Plugin interface
+func TestPathsChangedFulfillsPluginInterface(t *testing.T) {
 	_ = NewRouter(WithConvertPlugins(NewPathsChanged()))
 }
