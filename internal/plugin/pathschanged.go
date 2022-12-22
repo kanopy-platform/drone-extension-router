217a79d78cfa913a04f9c36b710f35ceef50b9f6
Yuzhou Liu <yuzhou.liu@mongodb.com>
2022-12-22 12:48:28 -0500

    add debug prints

--- internal/plugin/pathschanged.go
+++ internal/plugin/pathschanged.go
@@ -2,6 +2,7 @@ package plugin
 
 import (
 	"context"
+	"log"
 
 	"github.com/drone/drone-go/drone"
 	"github.com/drone/drone-go/plugin/converter"
@@ -19,9 +20,17 @@ func NewPathsChanged() *PathsChanged {
 }
 
 func (p *PathsChanged) Convert(ctx context.Context, req *converter.Request) (*drone.Config, error) {
+	log.Printf("YUZHOU DEBUG before conversion")
+	log.Printf(req.Config.Data)
+
 	// TODO: this type can be removed once the upstream pathschanged
 	// plugin supports reading the drone token directly
 	plugin := pathschanged.New(p.provider, &pathschanged.Params{Token: req.Token.Access})
 
-	return plugin.Convert(ctx, req)
+	config, err := plugin.Convert(ctx, req)
+
+	log.Printf("YUZHOU DEBUG after conversion")
+	log.Printf(req.Config.Data)
+
+	return config, err
 }
