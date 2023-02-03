7b489fdf158ee4f98dff3abd88de9cd72ee3f0c3
Colin Hoglund <colinhoglund@gmail.com>
2023-02-03 13:22:46 -0800

    add test

---
+++ internal/config/config_test.go
@@ -0,0 +1,33 @@
+package config_test
+
+import (
+	"testing"
+
+	"github.com/kanopy-platform/drone-extension-router/internal/config"
+	"github.com/stretchr/testify/assert"
+	"sigs.k8s.io/yaml"
+)
+
+func TestConfig(t *testing.T) {
+	data := `---
+defaults:
+  enable: true
+  pipeline:
+    node_selector:
+      instancegroup: drone
+    tolerations:
+    - key: dedicated
+      operator: Equal
+      value: drone
+      effect: NoSchedule
+pathschanged:
+  enable: true`
+
+	c := config.New()
+	assert.NoError(t, yaml.Unmarshal([]byte(data), c))
+
+	assert.True(t, c.Defaults.Enable)
+	assert.True(t, c.Pathschanged.Enable)
+	assert.Equal(t, "drone", c.Defaults.Pipeline.NodeSelector["instancegroup"])
+	assert.Equal(t, "drone", c.Defaults.Pipeline.Tolerations[0].Value)
+}
