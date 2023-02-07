bc3222cf7ccdb5a1748d7f6982336b8f00b42d5b
Colin Hoglund <colinhoglund@gmail.com>
2023-02-07 12:35:31 -0800

    add json tags

--- pkg/manifest/types.go
+++ pkg/manifest/types.go
@@ -13,8 +13,8 @@ type (
 	}
 
 	resource struct {
-		Kind         Kind                   `yaml:"kind"`
-		ResourceData map[string]interface{} `yaml:",inline"`
+		Kind         Kind                   `yaml:"kind" json:"kind"`
+		ResourceData map[string]interface{} `yaml:",inline" json:",inline"`
 	}
 
 	Toleration struct {
@@ -22,18 +22,18 @@ type (
 	}
 
 	Pipeline struct {
-		Kind         Kind                   `yaml:"kind"`
-		Type         string                 `yaml:"type,omitempty"`
-		Name         string                 `yaml:"name,omitempty"`
-		NodeSelector map[string]string      `yaml:"node_selector,omitempty"`
-		Tolerations  []Toleration           `yaml:"tolerations,omitempty"`
-		ResourceData map[string]interface{} `yaml:",inline"`
+		Kind         Kind                   `yaml:"kind" json:"kind"`
+		Type         string                 `yaml:"type,omitempty" json:"type,omitempty"`
+		Name         string                 `yaml:"name,omitempty" json:"name,omitempty"`
+		NodeSelector map[string]string      `yaml:"node_selector,omitempty" json:"node_selector,omitempty"`
+		Tolerations  []Toleration           `yaml:"tolerations,omitempty" json:"tolerations,omitempty"`
+		ResourceData map[string]interface{} `yaml:",inline" json:",inline"`
 	}
 
 	Secret struct {
-		Kind         Kind                   `yaml:"kind"`
-		Name         string                 `yaml:"name,omitempty"`
-		ResourceData map[string]interface{} `yaml:",inline"`
+		Kind         Kind                   `yaml:"kind" json:"kind"`
+		Name         string                 `yaml:"name,omitempty" json:"name,omitempty"`
+		ResourceData map[string]interface{} `yaml:",inline" json:",inline"`
 	}
 )
 
