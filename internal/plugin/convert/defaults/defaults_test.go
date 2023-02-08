faac23a64bbb11e709433e17566769928f5be58d
Colin Hoglund <colinhoglund@gmail.com>
2023-02-08 11:08:57 -0800

    updates tests

--- internal/plugin/convert/defaults/defaults_test.go
+++ internal/plugin/convert/defaults/defaults_test.go
@@ -7,121 +7,54 @@ import (
 	"github.com/drone/drone-go/drone"
 	"github.com/drone/drone-go/plugin/converter"
 	"github.com/kanopy-platform/drone-extension-router/internal/plugin/convert/defaults"
-	"github.com/kanopy-platform/drone-extension-router/pkg/manifest"
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
 )
 
 func TestDefaults(t *testing.T) {
 	tests := []struct {
 		desc    string
-		config  defaults.Config
-		request []manifest.Resource
-		want    []manifest.Resource
+		config  string
+		request string
+		want    string
 	}{
 		{
 			desc: "test with empty defaults and request",
 		},
 		{
 			desc:    "test without defaults",
-			request: []manifest.Resource{&manifest.Pipeline{Kind: "pipeline"}},
-			want:    []manifest.Resource{&manifest.Pipeline{Kind: "pipeline"}},
+			request: "kind: pipeline",
+			want:    "kind: pipeline",
 		},
 		{
-			desc: "test with defaults",
-			config: defaults.Config{
-				Pipeline: manifest.Pipeline{
-					NodeSelector: map[string]string{"d": "d", "test": "d"},
-					ResourceData: map[string]interface{}{
-						"map": map[string]interface{}{"d": "d", "test": "d"},
-						"list": []interface{}{
-							map[string]interface{}{"test": "d"},
-						},
-					},
-				},
-			},
-			request: []manifest.Resource{
-				&manifest.Pipeline{
-					Kind:         "pipeline",
-					NodeSelector: map[string]string{"r": "r", "test": "r"},
-					ResourceData: map[string]interface{}{
-						"map": map[string]interface{}{"r": "r", "test": "r"},
-						"list": []interface{}{
-							map[string]interface{}{"test": "r"},
-						},
-					},
-				},
-			},
-			want: []manifest.Resource{
-				&manifest.Pipeline{
-					Kind:         "pipeline",
-					NodeSelector: map[string]string{"r": "r", "d": "d", "test": "r"},
-					ResourceData: map[string]interface{}{
-						"map": map[string]interface{}{"r": "r", "d": "d", "test": "r"},
-						"list": []interface{}{
-							map[string]interface{}{"test": "r"},
-						},
-					},
-				},
-			},
+			desc:    "test with defaults",
+			config:  "pipeline:\n  kind: pipeline\n  node_selector:\n    d: d\n    test: d\n  map:\n    d: d\n    test: d\n  list:\n  - test: d",
+			request: "kind: pipeline\nnode_selector:\n  r: r\n  test: r\nmap:\n  r: r\n  test: r\nlist:\n- test: r",
+			want:    "kind: pipeline\nnode_selector:\n    d: d\n    r: r\n    test: r\nlist:\n    - test: r\nmap:\n    d: d\n    r: r\n    test: r",
 		},
 		{
-			desc: "test default node_selector and tolerations",
-			config: defaults.Config{manifest.Pipeline{
-				NodeSelector: map[string]string{"instancegroup": "drone"},
-				Tolerations: []manifest.Toleration{
-					{Key: "dedicated", Operator: "Equal", Value: "drone", Effect: "NoSchedule"},
-				},
-			}},
-			request: []manifest.Resource{
-				&manifest.Pipeline{
-					Kind:         "pipeline",
-					NodeSelector: map[string]string{"instancegroup": "batch"},
-					Tolerations: []manifest.Toleration{
-						{Key: "dedicated", Operator: "Equal", Value: "batch", Effect: "NoSchedule"},
-					},
-				},
-			},
-			want: []manifest.Resource{
-				&manifest.Pipeline{
-					Kind:         "pipeline",
-					NodeSelector: map[string]string{"instancegroup": "batch"},
-					Tolerations: []manifest.Toleration{
-						{Key: "dedicated", Operator: "Equal", Value: "batch", Effect: "NoSchedule"},
-					},
-				},
-			},
+			desc:    "test default node_selector and tolerations",
+			config:  "pipeline:\n  kind: pipeline\n  node_selector:\n    instancegroup: drone\n  tolerations:\n  - key: dedicated\n    operator: Equal\n    value: drone\n    effect: NoSchedule",
+			request: "kind: pipeline\nnode_selector:\n  instancegroup: batch\ntolerations:\n- key: dedicated\n  operator: Equal\n  value: batch\n  effect: NoSchedule",
+			want:    "kind: pipeline\nnode_selector:\n    instancegroup: batch\ntolerations:\n    - key: dedicated\n      operator: Equal\n      value: batch\n      effect: NoSchedule",
 		},
 		{
-			desc:   "test with multiple objects",
-			config: defaults.Config{Pipeline: manifest.Pipeline{Type: "test", Name: "test"}},
-			request: []manifest.Resource{
-				&manifest.Pipeline{Kind: "pipeline", Name: "user"},
-				&manifest.Secret{Kind: "secret", Name: "user"},
-			},
-			want: []manifest.Resource{
-				&manifest.Pipeline{Kind: "pipeline", Type: "test", Name: "user"},
-				&manifest.Secret{Kind: "secret", Name: "user"},
-			},
+			desc:    "test with multiple objects",
+			config:  "pipeline:\n  type: test\n  name: test",
+			request: "kind: pipeline\nname: user\n---\nkind: secret\nname: user",
+			want:    "kind: pipeline\ntype: test\nname: user\n---\nkind: secret\nname: user",
 		},
 	}
 
 	for _, test := range tests {
-		d := defaults.New(test.config)
+		c := defaults.Config{}
+		assert.NoError(t, yaml.Unmarshal([]byte(test.config), &c))
 
-		reqData, err := manifest.Encode(test.request)
+		req := &converter.Request{Config: drone.Config{Data: test.request}}
+
+		config, err := defaults.New(c).Convert(context.TODO(), req)
 		assert.NoError(t, err)
 
-		req := &converter.Request{
-			Config: drone.Config{
-				Data: reqData,
-			},
-		}
-
-		config, err := d.Convert(context.TODO(), req)
-		assert.NoError(t, err)
-
-		got, err := manifest.Decode(config.Data)
-		assert.NoError(t, err)
-		assert.Equal(t, test.want, got, test.desc)
+		assert.Equal(t, test.want, config.Data, test.desc)
 	}
 }
