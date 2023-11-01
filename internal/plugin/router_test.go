67d13ea38aa028ae150b584b9aef8b5ff7fc2700
Colin Hoglund <colinhoglund@gmail.com>
2023-11-01 16:05:07 -0700

    add support for validator

--- internal/plugin/router_test.go
+++ internal/plugin/router_test.go
@@ -2,10 +2,13 @@ package plugin
 
 import (
 	"context"
+	"errors"
+	"strings"
 	"testing"
 
 	"github.com/drone/drone-go/drone"
 	"github.com/drone/drone-go/plugin/converter"
+	"github.com/drone/drone-go/plugin/validator"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -47,5 +50,54 @@ func TestConvertRouter(t *testing.T) {
 		conf, err := test.router.Convert(context.Background(), req)
 		assert.NoError(t, err)
 		assert.Equal(t, test.want, conf.Data, test.description)
+	}
+}
+
+// stringValidate returns an error if the passed in string is not in the validator requests Config.Data
+type stringValidate struct {
+	s string
+}
+
+func newStringValidate(s string) *stringValidate {
+	return &stringValidate{s: s}
+}
+
+func (p *stringValidate) Validate(ctx context.Context, req *validator.Request) error {
+	if !strings.Contains(req.Config.Data, p.s) {
+		return errors.New(p.s)
+	}
+
+	return nil
+}
+
+func TestValidateRouter(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		description string
+		router      *Router
+		input       string
+		err         error
+	}{
+		{
+			description: "test without validate plugins",
+			router:      NewRouter(""),
+		},
+		{
+			description: "test passing validate plugin",
+			router:      NewRouter("", WithValidatePlugins(newStringValidate("one"))),
+			input:       "one",
+		},
+		{
+			description: "test with multiple validate plugins and an error",
+			router:      NewRouter("", WithValidatePlugins(newStringValidate("one"), newStringValidate("two"))),
+			input:       "one",
+			err:         errors.New("two"),
+		},
+	}
+
+	for _, test := range tests {
+		req := &validator.Request{Config: drone.Config{Data: test.input}}
+		assert.Equal(t, test.err, test.router.Validate(context.TODO(), req))
 	}
 }
